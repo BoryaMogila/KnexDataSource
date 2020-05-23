7c4dea2d2adfd7a26c54019bc778fb26ea5b01e1
boryam <borys.mohyla@ria.com>
2020-05-24 01:11:30 +0300

    init

--- index.js
+++ index.js
@@ -1,4 +1,68 @@
-const SQLCache = require("./SQLCache");
-const SQLDataSource = require("./SQLDataSource");
+const { InMemoryLRUCache } = require("apollo-server-caching");
+const { DataSource } = require("apollo-datasource");
+const crc = require('crc')
 
-module.exports = { SQLCache, SQLDataSource };
+const { DEBUG } = process.env;
+
+class KnexDataSource extends DataSource {
+  constructor(props) {
+    super(props);
+    this.cache = new InMemoryLRUCache();
+    this.memoizedResults = new Map();
+  }
+  initialize(config = {}) {
+    this.memoizedResults = new Map();
+    this.context = config.context;
+    this.cache = config.cache || this.cache;
+    if (!this.db) throw configError;
+  }
+
+  getCacheKey(queryString) {
+    return crc.crc32(`${queryString}`).toString(16)
+  }
+
+  getCacheString(ttl = 0, query) {
+    return `${query.toString()}${!ttl ? '' : `_${ttl}`}`
+  }
+
+  createLog(cacheString) {
+    return (res) => {
+      if (DEBUG) console.timeEnd(cacheString);
+      return res;
+    }
+  }
+
+  execute(query, ttl = 0) {
+    const cacheString = this.getCacheString(ttl, query);
+    if (DEBUG) console.time(cacheString);
+    const log = this.createLog(cacheString)
+    const cacheKey = this.getCacheKey(cacheString);
+    let promise = this.memoizedResults.get(cacheKey);
+    if (promise) return promise.then(log);
+    if (!ttl) {
+      promise = this.getResult(ttl, query, cacheKey);
+      this.memoizedResults.set(cacheKey, this.getResult(ttl, query, cacheKey));
+      return promise.then(log);
+    }
+    promise = this.cacheQuery(ttl, query, cacheKey);
+    this.memoizedResults.set(cacheKey, promise);
+    return promise.then(log);
+  }
+
+  getResult(ttl = 0, query, cacheKey) {
+    return query.then(rows => {
+      if (!ttl || !rows) return Promise.resolve(rows);
+      this.cache.set(cacheKey, JSON.stringify(rows), { ttl });
+      return Promise.resolve(rows);
+    });
+  }
+
+  cacheQuery(ttl = 0, query, cacheKey) {
+    return this.cache.get(cacheKey).then(entry => {
+      if (entry) return Promise.resolve(JSON.parse(entry));
+      return this.getResult(ttl, query, cacheKey);
+    });
+  }
+}
+
+module.exports = KnexDataSource;
--- index.test.js
+++ index.test.js
@@ -1,6 +1,29 @@
-// NOTE: Never use .toBe() for testing values use .toEqual()
-// https://jestjs.io/docs/en/expect#toequalvalue
+const knex = require('knex')({
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: "./test.sqlite"
+  }
+});
+
+const KnexDataSource = require('./index');
+
+class TestDataSource extends KnexDataSource {
+  constructor(knex) {
+    super();
+    this.db = knex;
+  }
+  getTestUsers() {
+    return this.execute(this.db.select().from('test'), 100)
+  }
+}
+
 
 describe("jest", () => {
-  test("is configured", () => expect(true).toBeTruthy());
+  test("is configured", async () => {
+    const testDataSource = new TestDataSource(knex);
+    testDataSource.initialize();
+    await expect(await testDataSource.getTestUsers()).toEqual([{ id: 1, name: 'Test' }])
+    await knex.destroy()
+  });
 });
